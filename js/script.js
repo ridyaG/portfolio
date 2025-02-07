2f79c0670f5113d3e7684150c5477808fc97a94a
ridyaG <95574918+ridyaG@users.noreply.github.com>
2025-02-07 12:16:29 +0000

    Made changes to the portfolio and deleted images.txt

--- js/script.js
+++ js/script.js
@@ -66,7 +66,7 @@ let reactProgress = document.querySelector(".reactjs"),
   reactValue = document.querySelector(".reactjs-progress");
 
 let reactStartValue = 0,
-  reactEndValue = 45,
+  reactEndValue = 70,
   rjsspeed = 30;
 
 let progressreact = setInterval(() => {
